0d8ed44907adcedafba0ee3b23966f294f525ada
Hoo Yu Heng <hooyuheng@gmail.com>
2017-12-26 18:06:20 +0800

    crawl successful

--- web_glossary_crawler.py
+++ web_glossary_crawler.py
@@ -1,29 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
+# download the page source fr URL below and convert to text
 url = 'https://www.analyticsvidhya.com/glossary-of-common-statistics-and-machine-learning-terms/'
 source_code = requests.get(url)
 code_in_text = source_code.text
 
-# crawling setup
+# list to store all extracted info
+glossary = []
+
+# use lxml parser to parse the code_in_text
 soup = BeautifulSoup(code_in_text, 'lxml')
 
-text_content = soup.find('div', {'class': 'text-content'})
+# get all tables tag
+tables = soup.find_all('table')
 
-for su_table in text_content.find_all('div', {'class': 'su-table'}):
-    table = su_table.find('table')
-    if table:
-        for rows in table.find_all('tr'):
-            first_column = rows.find('td')
-            if first_column:
-                print(first_column)
+# iterate through all tables tag and do:
+for table in tables:
+    for row in table.find_all('tr'):
+        column = row.find('td')
+        if column:
+            for bold_text in column.find_all('strong'):
+                info = bold_text.get_text()
+                print(info)
+                glossary.append(info.rstrip())  # remove the trailing char like \n
 
-# tables = soup.find_all('table')
-#
-# for table in tables:
-#     for row in table.find_all('tr'):
-#         column = row.find('td')
-#         if column:
-#             print(column)
+print(glossary)
+
+
 
 
