1ebb0527349517aa472ec0b48ebde6d48a9a147e
Hoo Yu Heng <hooyuheng@gmail.com>
2017-12-26 17:43:27 +0800

    still fail to parse the first column ONLY

--- web_glossary_crawler.py
+++ web_glossary_crawler.py
@@ -6,10 +6,24 @@ source_code = requests.get(url)
 code_in_text = source_code.text
 
 # crawling setup
-soup = BeautifulSoup(code_in_text, 'lxml')  
-tables = soup.find_all('table')
+soup = BeautifulSoup(code_in_text, 'lxml')
 
-for table in tables:
-    for row in table.find_all('td', {'style': 'text-align: center;'}):
-        print(row)
+text_content = soup.find('div', {'class': 'text-content'})
+
+for su_table in text_content.find_all('div', {'class': 'su-table'}):
+    table = su_table.find('table')
+    if table:
+        for rows in table.find_all('tr'):
+            first_column = rows.find('td')
+            if first_column:
+                print(first_column)
+
+# tables = soup.find_all('table')
+#
+# for table in tables:
+#     for row in table.find_all('tr'):
+#         column = row.find('td')
+#         if column:
+#             print(column)
+
 
