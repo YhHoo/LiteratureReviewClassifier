1c01ee367a310aa241a4f7786cf7ec7194adf7a9
Hoo Yu Heng <hooyuheng@gmail.com>
2017-12-31 21:48:47 +0800

    update

--- ml_glossary_frequency_counter.py
+++ ml_glossary_frequency_counter.py
@@ -5,8 +5,28 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
-from io import BytesIO, StringIO
+from io import StringIO
 import re
+
+
+# groups all mini glossaries of txt and save in a new txt
+def glossary_database_accumulate():
+    # extract all words from all mini glossaries
+    mini_glossary = ['ml_glossary_1.txt', 'ml_glossary_2.txt', 'ml_glossary_3.txt']
+    temp = []
+    for files in mini_glossary:
+        with open(files, 'r') as f:
+            for word in f:
+                # write all words in lowercase
+                temp.append(word.lower())
+    print('Extraction Completed !')
+    # sort alphabetically
+    temp.sort()
+    # save all extracted to another files
+    with open('ml_glossary_all.txt', 'w') as f:
+        for word in temp:
+            f.write(word)
+    print('Saving Completed !')
 
 
 def glossary_counter(glossary, pdf_string, visualize=False):
@@ -25,31 +45,10 @@ def glossary_counter(glossary, pdf_string, visualize=False):
         plt.plot(database, frequency)
 
 
-# groups all mini glossaries of txt and save in a new txt
-def glossary_database_accumulate():
-    # extract all words from all mini glossaries
-    mini_glossary = ['ml_glossary_1.txt', 'ml_glossary_2.txt', 'ml_glossary_3.txt']
-    temp = []
-    for files in mini_glossary:
-        with open(files, 'r') as f:
-            for word in f:
-                # write all words in lowercase
-                temp.append(word.lower())
-    print('Extraction Completed !')
-
-    # sort alphabetically
-    temp.sort()
-
-    # save all extracted to another files
-    with open('ml_glossary_all.txt', 'w') as f:
-        for word in temp:
-            f.write(word)
-    print('Saving Completed !')
-
-
 # PyPDF2
 def pdf_to_text_pypdf2(filename):
-    # temporary for testing the pdf miner
+    # Retrieved fr:
+    # https://stackoverflow.com/questions/32667398/best-tool-for-text-extraction-from-pdf-in-python-3-4
     pdf_filename = filename
     pdf_file_obj = open(pdf_filename, 'rb')
     pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
@@ -60,32 +59,31 @@ def pdf_to_text_pypdf2(filename):
 
 
 # Pdf Miner
-def pdf_to_text_pdfminer(filename):
+def pdf_to_text_pdfminer(filename, char_filter=False):
     pdf_filename = filename
     # PDFMiner boilerplate
     rsrcmgr = PDFResourceManager()
-    sio = StringIO()
+    sio = StringIO()  # if use ByteIO, the hex code appears in the text
     codec = 'utf-8'
     laparams = LAParams()
     device = TextConverter(rsrcmgr, sio, codec=codec, laparams=laparams)
     interpreter = PDFPageInterpreter(rsrcmgr, device)
-
     # extract text
     with open(pdf_filename, 'rb') as fp:
         for page in PDFPage.get_pages(fp):
             interpreter.process_page(page)
-
     # get text from ByteIO
     text = sio.getvalue()
-
     # close
     device.close()
     sio.close()
-
+    # char filter
+    if char_filter:
+        # remove all char inside []
+        text = re.sub('[({[\])’‘,.;:]', '', text)
+    # return in string
     return text
 
 
-pdf_text = pdf_to_text_pdfminer(filename='journal_test.pdf')
-# remove some characters
-pdf_text = re.sub('[({[\]),.\-;:]', '', pdf_text)
+pdf_text = pdf_to_text_pdfminer(filename='ML_in_human_migration.pdf', char_filter=True)
 print(pdf_text)
