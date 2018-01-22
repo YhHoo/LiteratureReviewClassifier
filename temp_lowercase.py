a59041b0c00cc27f208540ccd57fddda17cfce1e
Hoo Yu Heng <hooyuheng@gmail.com>
2018-01-22 18:35:16 +0800

    pandas writing

--- temp_lowercase.py
+++ temp_lowercase.py
@@ -1,14 +1,7 @@
+# for testing only
 
-mini_glossary = ['ml_glossary_all2.txt']
-temp = []
-for files in mini_glossary:
-    with open(files, 'r') as f:
-        for word in f:
-            # write all words in lowercase
-            temp.append(word.lower())
-print('Extraction Completed !')
-# save all extracted to another files
-with open('ml_glossary_all2X.txt', 'w') as f:
-    for word in temp:
-        f.write(word)
-print('Saving Completed !')
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('ml_glossary_all2_table.csv', index_col=[0])
+print(df.head())
